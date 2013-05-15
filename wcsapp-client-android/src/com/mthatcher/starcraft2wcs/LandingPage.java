8f1a732eb954532d585aef687c5f453acbf11207
Sam Procter <sam.procter@gmail.com>
2013-05-15 00:45:06 +0000

    Initial commit, "Hello World" template.

---
+++ wcsapp-client-android/src/com/mthatcher/starcraft2wcs/LandingPage.java
@@ -0,0 +1,23 @@
+package com.mthatcher.starcraft2wcs;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LandingPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.landing_page, menu);
+        return true;
+    }
+    
+}
